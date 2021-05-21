828913255fce1218ecf2bc3024406cc50c5468b7
lazuardykamil <lazuardy.kamil@gmail.com>
2021-05-21 15:54:18 +0700

    change another cors

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ app.use(multipart.any());
 /* eslint-disable no-unused-vars */
 app.use(async (req, res, next) => {
   try {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     req.Knex = Knex;
     req.Config = config;
     req.Fetch = fetch;
